59d7b886b79f45263d6a2e01344743e3b930564e
Svetlana <Svetlanat1005@yandex.ru>
2024-03-06 13:16:00 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создайте строку из символов этого массива.
+
+char[,] array = new char [,]
+{
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f'}
+};
+string str = "";
+
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        str = str + array[i,j];
+    }
+}
+Console.WriteLine(str);
