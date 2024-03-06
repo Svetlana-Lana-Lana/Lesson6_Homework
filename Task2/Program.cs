69f4c5024db1f2d92aebf7cf893372e8862b3811
Svetlana <Svetlanat1005@yandex.ru>
2024-03-06 13:32:08 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, 
+// в которой все заглавные буквы заменены на строчные.
+
+string str = "aBcD1ef!-";
+string LowStr = str.ToLower();
+Console.WriteLine(LowStr);
+
+//string UpStr = str.ToUpper();
+//Console.WriteLine(UpStr);
