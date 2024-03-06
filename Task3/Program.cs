9bac79cf1208903d02ccea16cea2a4682c76caf7
Svetlana <Svetlanat1005@yandex.ru>
2024-03-06 13:48:51 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом
+
+//string str = "aBcD1ef";
+string str = "556655";
+string answer = "Да"; 
+for(int i = 0; i < str.Length/2; i++)
+{
+    if(str[i]!=str[str.Length-1-i])
+    {
+        answer = "Нет";
+    }
+
+}
+Console.Write(answer);
